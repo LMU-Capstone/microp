9120eb2c0dd997246b2460898cc885e147c6cee6
igreen1 <iangreen2.00@gmail.com>
2022-02-16 20:20:43 -0800

    Finished first draft receiver

--- src/tracking_simulation/include/Receiver.hpp
+++ src/tracking_simulation/include/Receiver.hpp
@@ -4,18 +4,28 @@
 #include <iostream>
 
 #include "../include/global.hpp"
+#include "../include/VelocityPattern.hpp"
 
 class Receiver { 
     public:
-
+        Receiver(VelocityPattern velocityPattern){
+            this->set_pattern(velocityPattern);
+        }
         void tick(float);
         void reset();
+
+
+        void set_pattern(VelocityPattern newVelocityPattern){
+            this->velocityPattern = newVelocityPattern;
+        }
         
     //output and operator casting
     friend std::ostream& operator<<(std::ostream& os, Receiver& r) {
 		return os;
 	}
     private:
+        float x = 0, y = 0, past_time = 0;
+        VelocityPattern velocityPattern;
 };
 
 #endif
--- src/tracking_simulation/src/Receiver.cpp
+++ src/tracking_simulation/src/Receiver.cpp
@@ -10,10 +10,17 @@ Simulates the receiver in our algorithm
 
 void Receiver::tick(float current_time){
     // get velocity
+    auto velocity = this->velocityPattern.get_current_velocity(current_time);
     // get dt
+    auto dt = current_time - this->past_time;
     // update x
+    this->x += dt * velocity[0];
     // update y
+    this->y += dt * velocity[1];
 }
 
 void Receiver::reset(){
+    this->past_time = 0;
+    this->x = 0;
+    this->y = 0;
 }
